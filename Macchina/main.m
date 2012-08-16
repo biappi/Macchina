0e4f471a6844bf96880d2a61932f3ba45dffdc15
Biappi <antonio.malara@gmail.com>
2012-08-16 12:48:33 +0200

    Initial Commit

---
+++ Macchina/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Macchina
+//
+//  Created by Antonio "Willy" Malara on 16/08/12.
+//  Copyright (c) 2012 Antonio "Willy" Malara. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
